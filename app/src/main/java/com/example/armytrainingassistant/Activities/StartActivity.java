f04b530ea90bd92ccfbc940e50574d028c62f5b6
nscherer30 <nscherer30@gmail.com>
2020-04-11 18:56:53 -0500

    generally even starting layout

--- app/src/main/java/com/example/armytrainingassistant/Activities/StartActivity.java
+++ app/src/main/java/com/example/armytrainingassistant/Activities/StartActivity.java
@@ -1,6 +1,11 @@
 package com.example.armytrainingassistant.Activities;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +13,23 @@ import com.example.armytrainingassistant.R;
 
 public class StartActivity extends AppCompatActivity {
 
+    private ImageButton machineGunRangeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        machineGunRangeButton = (ImageButton) findViewById(R.id.machine_gun_button);
+        machineGunRangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
+        //TODO Create new range button icons
 
 
     }
