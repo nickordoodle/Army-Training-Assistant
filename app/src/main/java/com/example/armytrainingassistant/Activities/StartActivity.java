996e1c63041dded8381a33d54bf199ec99e95fa7
nscherer30 <nscherer30@gmail.com>
2020-04-11 19:16:48 -0500

    cleaner table view, buttons init

--- app/src/main/java/com/example/armytrainingassistant/Activities/StartActivity.java
+++ app/src/main/java/com/example/armytrainingassistant/Activities/StartActivity.java
@@ -14,6 +14,10 @@ import com.example.armytrainingassistant.R;
 public class StartActivity extends AppCompatActivity {
 
     private ImageButton machineGunRangeButton;
+    private ImageButton rifleGunRangeButton;
+    private ImageButton pistolGunRangeButton;
+    private ImageButton grenadeGunRangeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +25,47 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         machineGunRangeButton = (ImageButton) findViewById(R.id.machine_gun_button);
+        rifleGunRangeButton = (ImageButton) findViewById(R.id.m4_m16_gun_range_button);
+        pistolGunRangeButton = (ImageButton) findViewById(R.id.m9_m17_range_button);
+        grenadeGunRangeButton = (ImageButton) findViewById(R.id.m320_m203_range_button);
+
+        //TODO Create new range button icons
+
+        Intent mainActivityIntent = new Intent(StartActivity.this, MainActivity.class);
+
         machineGunRangeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(StartActivity.this, MainActivity.class);
-                startActivity(myIntent);
+
+                //TODO Add extra activity designator to determine which type of range
+                startActivity(mainActivityIntent);
             }
         });
 
-        //TODO Create new range button icons
+        rifleGunRangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Add extra activity designator to determine which type of range
+                startActivity(mainActivityIntent);
+            }
+        });
+
+        pistolGunRangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Add extra activity designator to determine which type of range
+                startActivity(mainActivityIntent);
+            }
+        });
+
+        grenadeGunRangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Add extra activity designator to determine which type of range
+                startActivity(mainActivityIntent);
+            }
+        });
+
 
 
     }
